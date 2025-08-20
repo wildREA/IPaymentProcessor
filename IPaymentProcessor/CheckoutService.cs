1ee8d0f9126134b93d771c0021655a70e82d6d3c
wildREA <142332516+wildREA@users.noreply.github.com>
2025-08-20 10:52:20 +0200

    Add project files.

---
+++ IPaymentProcessor/CheckoutService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor_Interface
+{
+    internal class CheckoutService
+    {
+        private readonly IPaymentProcessor _paymentProcessor;
+        public CheckoutService(IPaymentProcessor paymentProcessor)
+        {
+            _paymentProcessor = paymentProcessor;
+        }
+        public string Process(decimal amount)
+        {
+            return _paymentProcessor.Process(amount);
+        }
+    }
+}
---
+++ IPaymentProcessor/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PaymentProcessor_Interface
+{
+    public interface IPaymentProcessor
+    {
+        string Process(decimal amount);
+    }
+
+    public class PaymentProcessor : IPaymentProcessor
+    {
+        public string Process(decimal amount)
+        {
+            return $"Processing payment of {amount:C}";
+        }
+    }
+
+    public class StripePaymentProcessor : IPaymentProcessor
+    {
+        public string Process(decimal amount)
+        {
+            return $"Processing Stripe payment of {amount:C}";
+        }
+    }
+
+    public class PayPalPaymentProcessor : IPaymentProcessor
+    {
+        public string Process(decimal amount)
+        {
+            return $"Processing PayPal payment of {amount:C}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            
+            Console.WriteLine("Choose a payment processor (PayPal / Stripe):");
+            var register = Console.ReadLine()?.ToLowerInvariant();
+            
+            switch (register)
+            {
+                case "paypal":
+                    serviceCollection.AddTransient<IPaymentProcessor, PayPalPaymentProcessor>();
+                    break;
+                case "stripe":
+                    serviceCollection.AddTransient<IPaymentProcessor, StripePaymentProcessor>();
+                    break;
+                default:
+                    Console.WriteLine("Invalid payment processor specified.");
+                    return;
+            }
+
+            serviceCollection.AddTransient<CheckoutService>();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var checkoutService = serviceProvider.GetRequiredService<CheckoutService>();
+            Console.WriteLine(checkoutService.Process(100));
+        }
+    }
+}
